b17d1bd6566fe246592c25f51bfca4ec58cbd632
Grave Panda <thegravepanda@gmail.com>
2019-12-08 18:15:41 +0530

    it works + cleanup

--- index.js
+++ index.js
@@ -13,26 +13,18 @@ app.get('/himastermind/newgame/*', function (req, res) {
 	name = name.substring(0, (name.indexOf('/')<0?name.length:name.indexOf('/')));
 	let id = makeid(6);
 	let correct = Math.floor(Math.random()*1000000);
-	// history.set(id, [Math.floor(Math.random()*1000000), (Date.now()/1000 | 0)]);
 	pool.query('INSERT INTO data (key, correct, started) VALUES  ($1, $2, $3)', [id, correct, (Date.now()/1000 | 0)], error => {
 	    if (error) {
     	  	throw error;
     	}
     	res.send(id);
   	})
-	// console.log(history);
 });
 
 app.get('/himastermind/play/*', function (req, res) {
 	let params = req.url.substring(19);
 	code = params.substring(0, (params.indexOf('/')<0?params.length:params.indexOf('/')));
 	guess = params.substring(params.indexOf('/')<0?params.length:params.indexOf('/') + 1);
-	// if (history.has(code)) {
-	// 	let accuracy = getDigitsAccuracy(Array.from(history.get(code)[0].toString()), Array.from(guess));
-	// 	res.send(accuracy + ' ' + ((Date.now()/1000 | 0)-(history.get(code)[1])).toString());
-	// } else {
-	// 	res.send('error!');
-	// }
 	pool.query('SELECT * FROM data WHERE key=\''+code+'\';', (error, results) => {
     if (error) {
     	throw error
@@ -40,7 +32,6 @@ app.get('/himastermind/play/*', function (req, res) {
     if (results.length == 0) {
     	res.send('error');
     }
-    console.log(results);
 	let accuracy = getDigitsAccuracy(Array.from(results.rows[0].correct.toString()), Array.from(guess));
     res.send(accuracy + ' ' + ((Date.now()/1000 | 0)-results.rows[0].started).toString());
   })
